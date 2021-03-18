ee1cc4e53926fcda2ec431d61d19789e57930786
CristinaPH <76510387+CristinaPH@users.noreply.github.com>
2021-03-18 22:26:42 +0100

    update the current time and last updated

--- src/index.js
+++ src/index.js
@@ -1,34 +1,72 @@
 
+
+function formatDate (timestamp){
+//calculate the current date; 
+let date = new Date (); 
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let day = days[date.getDay()]; // day of the week;  Sunday - Saturday : 0 - 6
+let dayMth = date.getDate(); // day of the Month: 1-31; 
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let month = months[date.getMonth ()]; // month of the year: 0-11, January gives 0
+let hours = date.getHours();
+if (hours<10) {hours = `0${hours}`;  
+}
+let minutes = date.getMinutes();
+if (minutes<10) { minutes = `0${minutes}`;    
+} 
+console.log (date);
+return `${day} ${dayMth} ${month} @${hours}:${minutes}`;
+}
+
+
+function formatDateLastUpdate (timestamp){
+//calculate the date; 
+let timeLastUpdate = new Date (timestamp); 
+
+let hours = timeLastUpdate.getHours();
+if (hours<10) {hours = `0${hours}`;  
+}
+let minutes = timeLastUpdate.getMinutes();
+if (minutes<10) { minutes = `0${minutes}`;    
+} 
+return `${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
-    console.log (response.data);
+    //console.log (response.data.dt);
+
     let city = response.data.name; 
     let temperature = Math.round(response.data.main.temp);
     let temperatureFeelsLike = Math.round(response.data.main.feels_like);
     let humidity = Math.round(response.data.main.humidity)  ;
     let windSpeed = Math.round(response.data.wind.speed);
     let description = response.data.weather[0].description
+    let date = formatDate(response.data.dt*1000); 
+    let lastUpdate = formatDateLastUpdate(response.data.dt*1000); 
+
     
-    let temperatureElement = document.querySelector ("#temperature-now");
-    temperatureElement.innerHTML = temperature;
-
-   let temperatureRealFeel = document.querySelector ("#real-feel");
-    temperatureRealFeel.innerHTML = temperatureFeelsLike;
-  
-   let humidityElement = document.querySelector ("#humidity");
-    humidityElement.innerHTML = humidity;
-   
-    let windElement = document.querySelector ("#wind");
-    windElement.innerHTML = windSpeed;
-
     let cityElement = document.querySelector ("#current-city");
-    cityElement.innerHTML = city; 
-
+    let temperatureElement = document.querySelector ("#temperature-now");
+    let temperatureRealFeel = document.querySelector ("#real-feel");
+    let humidityElement = document.querySelector ("#humidity");
+    let windElement = document.querySelector ("#wind");
     let descriptionElement = document.querySelector("#weather-description");
+    let dateElement = document.querySelector ("#date");
+    let lastUpdateElement = document.querySelector ("#lastUpdate");
+    
+    cityElement.innerHTML = city; 
+    temperatureElement.innerHTML = `${temperature}°`;
+    temperatureRealFeel.innerHTML = temperatureFeelsLike;
+    humidityElement.innerHTML = humidity;
+    windElement.innerHTML = windSpeed;
     descriptionElement.innerHTML = description; 
- 
+    dateElement.innerHTML = date;  
+    lastUpdateElement.innerHTML = lastUpdate;  
 }
 
-let cityName = "Vienna";
+let cityName = "New York";
 let temperatureUnitCelsius = "metric";
 let apiKey = "8d17eb2f64026eb70f16e29b12bf932a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${temperatureUnitCelsius}` ;
